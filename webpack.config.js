4a96b0bcab1a3fbaa31d8b9e135199fbc60df45b
AntiHero <prophreaker@gmail.com>
2019-01-09 18:27:46 +0300

    images to dist folder add

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
@@ -33,7 +34,24 @@ module.exports = {
             "style-loader",
             "css-loader"
         ] 
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './images',
+        to: '../dist/images/',
+      }])
+    ]
 };
